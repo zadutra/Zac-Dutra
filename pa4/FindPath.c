ff2d6af7ee9e516e42973a56c70e69cfc0f44401
Zachary <zdutra@ucsc.edu>
2019-05-28 00:08:41 -0700

    update

--- pa4/FindPath.c
+++ pa4/FindPath.c
@@ -13,7 +13,6 @@ int main(int argc, char * argv[]){
    int V1 = 0;
    int V2 = 0;
    int linecount = 0;
-   Graph G;
    // check command line for correct number of arguments
    if( argc != 3 ){
       printf("Usage: %s <input file> <output file>\n", argv[0]);
@@ -31,24 +30,37 @@ int main(int argc, char * argv[]){
       printf("Unable to open file %s for writing\n", argv[2]);
       exit(1);
    }
-
+   fscanf(in, "%[^\n]", line);
+   int order = (int)line[0];
+   Graph G = newGraph(order);
    /* read each line of input file, then count and print tokens */
    for(int j = 0; fgets(line, MAX_LEN, in) != NULL; j++){
-       linecount++;
-      if(j==0){
-          int size = (int)line[0];
-          Graph G = newGraph(size);
-          continue;
-      }
-      else if(line[0] == 0 && line[2] == 0){
+      linecount++;
+      if(line[0] == 0 && line[2] == 0){
           break;
       }
       V1 = (int)line[0];
       V2 = (int)line[2];
       addEdge(G,V1,V2);
       }
-    fprintf(out, "%s\n", G);
+   //print out the graph
+    fprintf(out, "\n", G);
    
+   //now go through file and get pairs to search paths for
+   List GList = newList();
+   for(int j = 0; fgets(line, MAX_LEN, in) != NULL; j++){
+      if(line[0] == 0 && line[2] == 0){
+          break;
+      }
+      V1 = (int)line[0];
+      V2 = (int)line[2];
+      BFS(G, V1);
+      getPath(GList, G, V2);
+      fprintf(out, "The distance from %d to %d is %d\n", V1, V2, getDist(G,V2));
+      fprintf(out, "A shortest %d-%d path is: ", V1, V2);
+      printList(out, GList);
+      fprintf(out, "\n");
+   }
    /* close files */
    fclose(in);
    fclose(out);
