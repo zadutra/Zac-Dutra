fb961b6f1174efd24653a79b6b335ba57ea3c9fd
zadutra <49461642+zadutra@users.noreply.github.com>
2019-05-28 23:11:09 -0700

    Update GraphTest.c

--- pa4/GraphTest.c
+++ pa4/GraphTest.c
@@ -1,1 +1,79 @@
-//temp
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <signal.h>
+#include <setjmp.h>
+
+#include "Graph.h"
+#include "List.h"
+int main(int argc, char * argv[]){
+  //source
+if (getSource(A) != NIL) return 1;
+        BFS(A, 42);
+        if (getSource(A) != 42) return 2;
+        BFS(A, 88);
+        if (getSource(A) != 88) return 3;
+        return 0;
+  //parent
+  for (uint8_t i = 1; i <= 100; i++)
+          if (getParent(A, i) != NIL) return 1;
+        addArc(A, 64, 4);
+        addArc(A, 64, 3);
+        addArc(A, 42, 2);
+        addArc(A, 2, 64);
+        addArc(A, 4, 2);
+        addArc(A, 3, 42);
+        BFS(A, 42);
+        if (getParent(A, 42) != NIL) return 2;
+        if (getParent(A, 2) != 42) return 3;
+        if (getParent(A, 8) != NIL) return 4;
+        return 0;
+  //dist
+  for (uint8_t i = 1; i <= 100; i++)
+          if (getDist(A, i) != INF) return 1;
+        addArc(A, 64, 4);
+        addArc(A, 64, 3);
+        addArc(A, 42, 2);
+        addArc(A, 2, 64);
+        addArc(A, 4, 2);
+        addArc(A, 3, 42);
+        BFS(A, 64);
+        if (getDist(A, 64) != 0) return 2;
+        if (getDist(A, 2) != 2) return 3;
+        BFS(A, 4);
+        if (getDist(A, 42) != 4) return 4;
+        if (getDist(A, 43) != INF) return 5;
+        BFS(A, 99);
+        if (getDist(A, 64) != INF) return 6;
+        return 0;
+  //getpath
+  addArc(A, 64, 4);
+        addArc(A, 64, 3);
+        addArc(A, 42, 2);
+        addArc(A, 2, 64);
+        addArc(A, 4, 2);
+        addArc(A, 3, 42);
+        BFS(A, 3);
+        getPath(L, A, 64);
+        append(C, 3);
+        append(C, 42);
+        append(C, 2);
+        append(C, 64);
+        if (!equals(L, C)) return 1;
+        moveFront(L);
+        BFS(A, 2);
+        getPath(L, A, 2);
+        append(C, 2);
+        if (!equals(L, C)) return 2;
+        getPath(L, A, 99);
+        if (equals(L, C)) return 3;
+        clear(L);
+        clear(C);
+        append(C, NIL);
+        BFS(A, 99);
+        getPath(L, A, 2);
+        if (!equals(C, L)) return 4;
+        return 0;
+}
