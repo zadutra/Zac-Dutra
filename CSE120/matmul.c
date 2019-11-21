8491bef38207e5bbf69341d9d02fcc8a80f86ba2
Zachary <zdutra@ucsc.edu>
2019-11-21 13:52:30 -0800

    update

--- CSE120/matmul.c
+++ CSE120/matmul.c
@@ -78,14 +78,14 @@ int main(int argc, char **argv)
 						matmul(A,B);
 							for(int i = 0; i < SIZE; i++){
 								for(int j = 0; j < SIZE; j++){
-									printf("%d " C[i][j]);
+									printf("%d ", C[i][j]);
 								}
 								printf("\n");
 							}
 							matmul(D, E);
 							for(int i = 0; i < SIZE; i++){
 								for(int j = 0; j < SIZE; j++){
-									printf("%d " C[i][j]);
+									printf("%d ", C[i][j]);
 								}
 								printf("\n");
 							}
