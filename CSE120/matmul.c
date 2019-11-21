a13fcd7c43e5f0cc3cd217803c79ed6e265bff1d
Zachary <zdutra@ucsc.edu>
2019-11-21 15:08:09 -0800

    update

--- CSE120/matmul.c
+++ CSE120/matmul.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define SIZE 1024
+#define SIZE 4
 
 volatile __uint64_t A[SIZE][SIZE];
 volatile __uint64_t B[SIZE][SIZE];
@@ -119,7 +119,19 @@ int main(int argc, char **argv)
 					memset((__uint64_t**)C, 0, sizeof(__uint64_t) * SIZE * SIZE);
 						t = clock();
 							matmul(A,B);
+							for(int i = 0; i < SIZE; i++){
+								for(int j = 0; j < SIZE; j++){
+									printf("%d ", D[i][j]);
+								}
+								printf("\n");
+							}
 							Til_matmul(A, B);
+							for(int i = 0; i < SIZE; i++){
+								for(int j = 0; j < SIZE; j++){
+									printf("%d ", C[i][j]);
+								}
+								printf("\n");
+							}
 							verify(C,D);
 								t = clock() - t;
 									time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
