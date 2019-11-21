0a7081658054ff083427cc1d868acc4463374768
Zachary <zdutra@ucsc.edu>
2019-11-21 13:27:42 -0800

    update

--- CSE120/matmul.c
+++ CSE120/matmul.c
@@ -9,6 +9,8 @@
 volatile __uint64_t A[SIZE][SIZE];
 volatile __uint64_t B[SIZE][SIZE];
 volatile __uint64_t C[SIZE][SIZE];
+volatile __uint64_t D[SIZE][SIZE];
+volatile __uint64_t E[SIZE][SIZE];
 
 void init(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE])
 {
@@ -17,11 +19,22 @@ void init(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE])
 			for (c = 0; c < SIZE; c++) {
 						for (r = 0; r < SIZE; r++) {
 										A[r][c] = rand();
-													B[r][c] = rand();
-															}
+										B[r][c] = rand();
+								}
+							}
+			transpose(D,A);
+			transpose(E,B);
+}
+void transpose(volatile __uint64_t D[][SIZE], volatile __uint64_t A[][SIZE])
+{
+		int r, c;
+
+			for (c = 0; c < SIZE; c++) {
+						for (r = 0; r < SIZE; r++) {
+										D[r][c] = A[c][r];
+								}
 							}
 }
-
 int verify(volatile __uint64_t C[][SIZE], volatile __uint64_t D[][SIZE])
 {
 		int r, c;
@@ -45,8 +58,8 @@ void matmul(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE])
 			for (rowA = 0; rowA < SIZE; rowA++) {
 						for (colB = 0; colB < SIZE; colB++) {
 										for (idx = 0; idx < SIZE; idx++) {
-															C[rowA][colB] += A[rowA][idx] * B[idx][colB];
-																		}
+												C[rowA][colB] += A[rowA][idx] * B[idx][colB];
+													}
 												}
 							}
 }
