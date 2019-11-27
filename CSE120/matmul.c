e952d2de327caffb073a9bfeb69a710e3c0002c5
Zachary <zdutra@ucsc.edu>
2019-11-26 20:39:34 -0800

    update

--- CSE120/matmul.c
+++ CSE120/matmul.c
@@ -100,7 +100,7 @@ int main(int argc, char **argv)
 				init(A, B);
 					memset((__uint64_t**)C, 0, sizeof(__uint64_t) * SIZE * SIZE);
 						t = clock();
-							Tile_matmul(A,B,1);
+							Tile_matmul(A,B,16);
 								t = clock() - t;
 									time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
 										
