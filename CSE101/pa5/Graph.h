f3943208f5a03d9ef4d74cb3cf996dd21388239f
zadutra <49461642+zadutra@users.noreply.github.com>
2019-12-03 08:14:18 -0800

    Create Graph.h

---
+++ CSE101/pa5/Graph.h
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+// Graph.h
+// Zachary Dutra
+// zdutra
+// 1581789
+// pa5
+//-----------------------------------------------------------------------------
+#ifndef _Graph_H_INCLUDE
+#define _Graph_H_INCLUDE
+#include "List.h"
+#define NIL -1
+#define UNDEF -2
+
+typedef struct GraphObj* Graph;
+/*** Constructors-Destructors ***/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+/*** Access functions ***/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getParent(Graph G, int u);
+int getDiscover(Graph G, int u);
+int getFinish(Graph G, int u);
+/*** Manipulation procedures ***/
+void makeNull(Graph G);
+void addEdge(Graph G, int u, int v);
+void addArc(Graph G, int u, int v);
+void DFS(Graph G, List s);
+void visit(Graph G, List S, int x);
+/*** Other operations ***/
+Graph transpose(Graph G);
+Graph copyGraph(Graph G);
+void printGraph(FILE* out, Graph G);
+
+#endif
