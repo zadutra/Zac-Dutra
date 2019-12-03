9246fdadecd8d4515219f45d2a03e5c311b84f45
zadutra <49461642+zadutra@users.noreply.github.com>
2019-12-03 07:59:24 -0800

    Create ListTest.c

---
+++ CSE101/pa2/ListTest.c
@@ -0,0 +1,14 @@
+/*
+Zachary Dutra
+zdutra
+1581789
+pa2
+*/ 
+
+class ListTest{
+    public static void main(String[] args){
+        List testList = new List();
+        int temp = 1;
+        testList.append(temp);
+    }
+}
