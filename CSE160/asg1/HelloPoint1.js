d66a502a617d668e10adcb0a02929b0005804843
zadutra <49461642+zadutra@users.noreply.github.com>
2020-04-11 12:37:15 -0700

    Update HelloPoint1.js

--- CSE160/asg1/HelloPoint1.js
+++ CSE160/asg1/HelloPoint1.js
@@ -1,16 +1,17 @@
  // HelloPoint1.js
   // Vertex shader program
   var VSHADER_SOURCE =
+      'attribute vec4 a_Position;\n'+
   'void main() {\n' +
-  ' gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Coordinates
+  ' gl_Position = a_Position;\n' + // Coordinates
   ' gl_PointSize = 10.0;\n' + // Set the point size
   '}\n';
  
   // Fragment shader program
- var FSHADER_SOURCE =
- 'void main() {\n' +
- ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the color
- '}\n';
+ var FSHADER_SOURCE = `
+ void main() {
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+ }`
 
  function main() {
  // Retrieve <canvas> element
@@ -28,7 +29,16 @@
  console.log('Failed to initialize shaders.');
  return;
  }
+  
+ //Get the storage location of a_Position
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if(a_Position < 0){
+   console.log('Failed to get the storage location of a_Position');
+  }
 
+ //Pass vertex position to attribute variable
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+  
  // Set the color for clearing <canvas>
  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
