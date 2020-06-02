9e2a8e546e4e4eeb8b6b86088025d766766631ba
zadutra <49461642+zadutra@users.noreply.github.com>
2020-06-02 13:00:37 -0700

    Create Cube.js

---
+++ CSE160/asg5/Cube.js
@@ -0,0 +1,56 @@
+class Cube{
+
+    constructor(){
+      this.type = 'cube';
+      //this.position = [0.0, 0.0, 0.0];
+      this.color = [1.0, 1.0, 1.0, 1.0];
+      //this.size = 5.0;
+      //this.segment = 10.0;
+      this.matrix = new Matrix4();
+      this.textureNum = 2;
+    }
+  
+    render(){
+      //var xy = this.position;
+      var rgba = this.color;
+      //var segments = this.segment;
+      
+      //pass in texture number
+      gl.uniform1i(u_whichTexture, this.textureNum);
+
+      //pass the color to u_FragColor
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+      //pass the model matrix attribute
+      gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+      
+      // Top of cube
+      draw3DTriangleUVNormal([0,1,0, 0,1,1, 1,1,1], [0,0, 0,1, 1,1], [0,1,0, 0,1,0, 0,1,0]);
+      draw3DTriangleUVNormal([0,1,0, 1,1,1, 1,1,0], [0,0, 1,1, 1,0], [0,1,0, 0,1,0, 0,1,0]);
+      
+      //left of cube
+      draw3DTriangleUVNormal([0,1,0, 0,1,1, 0,0,0], [0,0, 0,1, 1,1], [-1,0,0, -1,0,0, -1,0,0]);
+      draw3DTriangleUVNormal([0,0,0, 0,1,1, 0,0,1], [0,0, 1,1, 1,0], [-1,0,0, -1,0,0, -1,0,0]);
+
+      //bottom of cube
+      draw3DTriangleUVNormal([0,0,0, 0,0,1, 1,0,1], [0,0, 0,1, 1,1], [0,-1,0, 0,-1,0, 0,-1,0]);
+      draw3DTriangleUVNormal([0,0,0, 1,0,1, 1,0,0], [0,0, 1,1, 1,0], [0,-1,0, 0,-1,0, 0,-1,0]);
+
+      //front of cube
+      draw3DTriangleUVNormal([0,0,0, 0,1,0, 1,1,0], [0,0, 0,1, 1,1], [0,0,-1, 0,0,-1, 0,0,-1]);
+      draw3DTriangleUVNormal([0,0,0, 1,1,0, 1,0,0], [0,0, 1,1, 1,0], [0,0,-1, 0,0,-1, 0,0,-1]);
+
+      //back of cube
+      draw3DTriangleUVNormal([1,0,1, 0,0,1, 0,1,1 ], [0,0, 1,0, 1,1], [0,0,1, 0,0,1, 0,0,1]);
+      draw3DTriangleUVNormal([1,0,1, 0,1,1, 1,1,1 ], [1,0, 1,1, 0,1], [0,0,1, 0,0,1, 0,0,1]);
+      
+      //pass the color of a point to u_FragColor uniform variable
+      //gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+
+      //right of cube
+      draw3DTriangleUVNormal([1,1,0, 1,1,1, 1,0,0], [0,0, 0,1, 1,1], [1,0,0, 1,0,0, 1,0,0]);
+      draw3DTriangleUVNormal([1,0,0, 1,1,1, 1,0,1], [0,0, 1,1, 1,0], [1,0,0, 1,0,0, 1,0,0]);
+
+      }
+}
