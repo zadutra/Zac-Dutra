c10dcc335631447b905b2d4e0f1da8ae845a7dd2
Zachary <zdutra@ucsc.edu>
2020-10-16 10:29:54 -0700

    hw3 start

---
+++ CSE183/hw3/templater.js
@@ -0,0 +1,25 @@
+/**
+ * CSE183 Assignment 3 - Basic
+ */
+class Templater {
+  /**
+   * Create a templater
+   * @param {string} template - A {{ }} tagged string
+   */
+  constructor(template) {
+  }
+
+  /**
+   * Apply map to template to generate string
+   * @param {object} map Object with propeties matching tags in template
+   * @param {boolean} strict Throw an Error if any tags in template are
+   *     not found in map
+   * @return {string} template with all tags replaced
+   * @throws An Error if strict is set and any tags in template are not
+   *     found in map
+   */
+  apply(map, strict) {
+  }
+}
+
+module.exports = Templater;
---
+++ CSE183/hw3/templater.test.js
@@ -0,0 +1,29 @@
+const Templater = require('./templater');
+
+test('Undefined', () => {
+  const t = new Templater(undefined);
+  expect(t.apply({})).toBe(undefined);
+});
+
+test('Single Tag', () => {
+  const t = new Templater('Hello {{tag}}');
+  expect(t.apply({tag: 'World'})).toBe('Hello World');
+});
+
+test('Multi Tag', () => {
+  const t = new Templater('Mary {{had}} a {{little}} {{lamb}}');
+  expect(t.apply({had: 'had', little: 'little', lamb: 'lamb'}))
+      .toBe('Mary had a little lamb');
+});
+
+test('Missing Tag', () => {
+  const t = new Templater('Mary {{had}} a {{little}} {{lamb}}');
+  expect(t.apply({had: 'had', lamb: 'lamb'}))
+      .toBe('Mary had a lamb');
+});
+
+test('Missing Tag Strict', () => {
+  const t = new Templater('Mary {{had}} a {{little}} {{lamb}}');
+  expect(() => t.apply({had: 'had', lamb: 'lamb'}, true))
+      .toThrowError();
+});
